f92f785f9ef3f17fa675f2f84f3f3f8cb93c0cd2
Nawaraj Bista <Nawaraj Bista>
2016-06-23 20:23:20 -0500

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,34 @@
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+$(document).ready(function(){
+  $(window).scroll(function() {
+    wScroll = $(this).scrollTop();
+    if (wScroll > 300) {
+
+      $('.moveUp').css({
+        //'display': 'block',
+        'opacity': 1
+      });
+    }
+    else {
+      $('.moveUp').css({
+        //'display': 'none',
+        'opacity': 0
+      });
+    }
+  });
+});
